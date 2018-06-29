fd6a27c9223611e0f74a1f2f8bbddef49c78b0c0
alfiansyahrul <syahrul.alfian1@gmail.com>
2018-06-30 04:29:34 +0700

    update about and info

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,8 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About KamusApp Indo', desc: "inikamus" });
 });
 
+router.get('/info', function(req, res, next) {
+  res.render('info', { title: 'Info KamusApp Indo', desc: "inikamus" });
+});
+
 module.exports = router;
